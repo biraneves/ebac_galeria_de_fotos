6f561739946c16ddf1756994c4fd2a96df0b9aee
Ubirajara Neves <biraneves@outlook.com>
2024-01-09 18:39:49 +0100

    Adicionados os comportamentos da galeria de imagens

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,19 @@ $(document).ready(() => {
 
   $('form').on('submit', (e) => {
     e.preventDefault();
+
+    const enderecoImagem = $('#url-imagem').val();
+    const novoItem = $('<li style="display: none;"></li>');
+    $(`<img src="${enderecoImagem}">`).appendTo(novoItem);
+    $(`
+      <div class="overlay-imagem-link">
+        <a href="${enderecoImagem}" title="Ver imagem em tamanho real" target="_blank">
+          Ver imagem em tamanho real
+        </a>
+      </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn();
+    $('#url-imagem').val('');
   });
 });
